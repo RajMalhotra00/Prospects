741f545ca07eda83710edf6e220ef14dc53d6ecc
RajMalHotRA123 <RM_frankfurt@gmx.de>
2021-04-05 18:35:53 +0200

    Initial Commit

---
+++ Prospects3/Prospects3App.swift
@@ -0,0 +1,17 @@
+//
+//  Prospects3App.swift
+//  Prospects3
+//
+//  Created by Raj Malhotra on 05.04.21.
+//
+
+import SwiftUI
+
+@main
+struct Prospects3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
